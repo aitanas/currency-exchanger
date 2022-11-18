b30754ea32d913d8cb25c707eb1d2352c4df4501
Aitana Shough <aeshough@gmail.com>
2022-11-18 09:01:35 -0800

    add printConversion and printError functions

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function exchange(currency1, currency2, amount) {
   CurrencyExchange.exchange(currency1, currency2, amount)
     .then(function(response) {
       if (response) {
-        printElements(response, currency1, currency2, amount);
+        printConversion(response, currency1, currency2, amount);
       } else {
         printError(response, currency1, currency2, amount);
       }
@@ -18,3 +18,17 @@ function exchange(currency1, currency2, amount) {
 
 
 // UI Logic
+
+function printConversion(response, currency1, currency2, amount) {
+  document.querySelector('#response').innerText = `${amount}${currency1} exchanged to ${currency2} comes out to ${response.conversion_result}${currency2}.`
+}
+
+function printError(error, currency1, currency2, amount) {
+  document.querySelector('#response').innerText = `There was an error accessing the currency exchange data for ${currency1} and/or ${currency2}:
+  ${response[`error-type`]}.`
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+}
