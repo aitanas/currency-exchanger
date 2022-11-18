43529345261c12f0e9f1fbbe8c21601206adb6ad
Aitana Shough <aeshough@gmail.com>
2022-11-18 08:50:39 -0800

    add setup files and package.json files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './js/currency-exchanger';
+
+// Business Logic
+
+function exchange(currency1, currency2, amount) {
+  CurrencyExchange.exchange(currency1, currency2, amount)
+    .then(function(response) {
+      if (response) {
+        printElements(response, currency1, currency2, amount);
+      } else {
+        printError(response, currency1, currency2, amount);
+      }
+    });
+}
+
+
+// UI Logic
--- src/js/currency-exchanger.js
+++ src/js/currency-exchanger.js
@@ -1,6 +1,6 @@
 export default class CurrencyExchange {
   static exchange(currency1, currency2, amount) {
-    return fetch(``)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
     .then(function(response) {
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
